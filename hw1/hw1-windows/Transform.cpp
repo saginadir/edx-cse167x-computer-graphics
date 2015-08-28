c714f17d302ec27a7a026693c91e721c61fa9b27
Stephen Merendino <stephenmerendino@gmail.com>
2015-08-28 07:07:39 -0500

    Finished hw1

--- hw1/hw1-windows/Transform.cpp
+++ hw1/hw1-windows/Transform.cpp
@@ -10,26 +10,38 @@
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
 	float rad = degrees * pi / 180.0;
 	return cos(rad) * mat3() 
-			+ (1 - cos(rad)) * mat3(axis.x * axis.x, axis.x * axis.y, axis.x * axis.z, axis.x * axis.y, axis.y * axis.y, axis.y * axis.z, axis.z * axis.x, axis.y * axis.z, axis.z * axis.z)
+			+ (1 - cos(rad)) * mat3(axis.x * axis.x, 
+									axis.x * axis.y, 
+									axis.x * axis.z, axis.x * axis.y, 
+													 axis.y * axis.y, 
+													 axis.y * axis.z, axis.z * axis.x, 
+																	  axis.y * axis.z, 
+																	  axis.z * axis.z)
 			+ sin(rad) * mat3(0, axis.z, -axis.y, -axis.z, 0, axis.x, axis.y, -axis.x, 0);
 }
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
-	eye = eye * rotate(degrees, glm::normalize(up));
+	eye = eye * rotate(-degrees, glm::normalize(up));
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
 	vec3 w = glm::normalize(glm::cross(eye, up));
-	eye = eye * rotate(degrees, w);
-	up = up * rotate(degrees, w);
+	eye = eye * rotate(-degrees, w);
+	up = up * rotate(-degrees, w);
 }
 
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
+	vec3 w = glm::normalize(eye);
+	vec3 u = glm::normalize(glm::cross(up, w));
+	vec3 v = glm::cross(w, u);
 
-  return mat4();
+	mat4 rot = mat4(u.x, v.x, w.x, 0, u.y, v.y, w.y, 0, u.z, v.z, w.z, 0, 0, 0, 0, 1);
+	mat4 trans = mat4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -eye.x, -eye.y, -eye.z, 1);
+
+	return rot * trans;
 }
 
 Transform::Transform()
